a384bf4be03f7927bcd005c8942675e191663244
1j23d <112849231+1j23d@users.noreply.github.com>
2024-03-07 21:25:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -143,15 +143,17 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         "touser": to_user,
         "template_id": config["template_id"],
         "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
+        # "topcolor": "#FF0000",
+        "topcolor": "#00FF00",
         "data": {
             "date": {
                 "value": "{} {}".format(today, week),
                 "color": random_color()
             },
             "region": {
-                # "value": region_name,
-                 "value": "<font color=\"#0000FF\">region_name</font>"
+                "value": region_name,
+                "color": "#00FF00"
+                # "value": "<font color=\"#0000FF\">region_name</font>"
                 # "color": "#FF0000"
             },
             "weather": {
